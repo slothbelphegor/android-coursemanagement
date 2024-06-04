a81f004c96857c4fbf281354856271e94e1e2c72
manle423 <trungmanle.vm@gmail.com>
2024-06-04 15:03:56 +0700

    check

--- app/src/main/java/com/example/qlkhoahoc/methods/course/findCourse.kt
+++ app/src/main/java/com/example/qlkhoahoc/methods/course/findCourse.kt
@@ -8,76 +8,31 @@ import retrofit2.Callback
 import retrofit2.Response
 
 fun findCourse(searchTerm: String, condition: String, callback: (MutableList<Course>) -> Unit) {
-    val courseNull = mutableListOf<Course>(
+    val courseNull = mutableListOf(
         Course(
             "", "Không tìm thấy", "Không tìm thấy", "Không tìm thấy",
             "Không tìm thấy", 0, 0, 0, "Không tìm thấy"
         )
     )
-    var rs: MutableList<Course>
-    if (condition == "false"){
-        val call = ApiClient.apiService.findCourse(searchTerm, condition = "")
-        call.enqueue(object : Callback<MutableList<Course>> {
-            override fun onResponse(
-                call: Call<MutableList<Course>>,
-                response: Response<MutableList<Course>>
-            ) {
-                if (response.isSuccessful) {
-                    val r = response.body()
-                    Log.d("check", r.toString())
-                    rs = if (r != null && r.isNotEmpty()) {
-                        r
-                    } else {
-                        courseNull
-                    }
-                    callback.invoke(rs)
-                    Log.d("Find", "Success Response")
-                } else {
-                    Log.d("Find", "Failed Response")
-                    Log.d("Response", response.toString())
-                    callback.invoke(courseNull)
-                }
+
+    val call = ApiClient.apiService.findCourse(searchTerm, condition)
+    call.enqueue(object : Callback<MutableList<Course>> {
+        override fun onResponse(
+            call: Call<MutableList<Course>>,
+            response: Response<MutableList<Course>>
+        ) {
+            val rs = if (response.isSuccessful && response.body().orEmpty().isNotEmpty()) {
+                Log.e("Error", condition)
+                response.body()!!
+            } else {
+                courseNull
             }
+            callback.invoke(rs)
+        }
 
-
-            override fun onFailure(call: Call<MutableList<Course>>, t: Throwable) {
-                Log.d("Find", "Failed")
-                callback.invoke(courseNull)
-            }
-
-        })
-    }
-    else {
-        val call = ApiClient.apiService.findCourse(searchTerm, condition)
-        call.enqueue(object : Callback<MutableList<Course>> {
-            override fun onResponse(
-                call: Call<MutableList<Course>>,
-                response: Response<MutableList<Course>>
-            ) {
-                if (response.isSuccessful) {
-                    val r = response.body()
-                    Log.d("check", r.toString())
-                    rs = if (r != null && r.isNotEmpty()) {
-                        r
-                    } else {
-                        courseNull
-                    }
-                    callback.invoke(rs)
-                    Log.d("Find", "Success Response")
-                } else {
-                    Log.d("Find", "Failed Response")
-                    Log.d("Response", response.toString())
-                    callback.invoke(courseNull)
-                }
-            }
-
-
-            override fun onFailure(call: Call<MutableList<Course>>, t: Throwable) {
-                Log.d("Find", "Failed")
-                callback.invoke(courseNull)
-            }
-
-        })
-    }
-
+        override fun onFailure(call: Call<MutableList<Course>>, t: Throwable) {
+            Log.d("Find", "Failed")
+            callback.invoke(courseNull)
+        }
+    })
 }
